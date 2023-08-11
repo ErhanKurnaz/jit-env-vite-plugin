25bbf72e1b083bfaa07c1680dc01a6120e1c85fc
Paul Gerarts <paulgerarts@weave.nl>
2023-08-11 13:14:08 +0200

    chore: finish v1 package

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import type { PluginOption } from "vite";
 import JitEnv, { JitEnvOptions } from "./jit-env";
 
-export default function pluginJitEnv(options: JitEnvOptions): PluginOption {
-  // TODO: pass the proper requestUpdate function
-  const jitEnv = new JitEnv(options, () => null);
+export default function jitEnv(options: JitEnvOptions): PluginOption {
+  // Tmp noop until restart can be set
+  let reload = () => {};
+
+  // Create the JitEnv instance to transform HTML files
+  const jitEnv = new JitEnv(options, () => {
+    reload();
+  });
+
   return {
     name: "vite-plugin-jit-env",
     version: "1.0",
+
+    // HTML transform hook
     transformIndexHtml(html) {
       return jitEnv.transform(html);
     },
+
+    configureServer(server) {
+      reload = server.restart;
+    }
   };
 }
